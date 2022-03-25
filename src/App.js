7f1a6bec574c3ea7d116d9621655b493da94f320
alex <HuiF8chi>
2022-03-25 13:27:40 +0200

    hook: useMemo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {Component, useState, useEffect, useCallback} from 'react';
+import {Component, useState, useEffect, useCallback, useMemo} from 'react';
 import {Container} from 'react-bootstrap';
 import './App.css';
 // class Slider extends Component {
@@ -54,6 +54,11 @@ import './App.css';
 //     }
 // }
 
+const countTotal = (num) => {
+    console.log('counting...');
+    return num + 10;
+}
+
 const Slider = (props) => {
 
     const [slide, setSlide] = useState(0);
@@ -91,11 +96,25 @@ const Slider = (props) => {
     function toggleAutoplay() {
         setAutoplay(autoplay => !autoplay);
     }
+
+    const total = useMemo(() => {
+        return countTotal(slide);
+    }, [slide]);
+
+    const style = useMemo(() => ({
+        color: slide > 4 ? 'red' : 'black'
+    }), [slide]);
+
+    useEffect(() => {
+        console.log('styles!');
+    }, [style]);
+
     return (
         <Container>
             <div className="slider w-50 m-auto">
                 <Slide getSomeImages={getSomeImages}/>
                 <div className="text-center mt-5">Active slide {slide} <br/>{autoplay ? 'auto' : null} </div>
+                <div style={style} className="text-center mt-5">Total slides: {total}</div>
                 <div className="buttons mt-3">
                     <button 
                         className="btn btn-primary me-2"
