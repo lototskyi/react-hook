81bb7b45b9af80b006dbfed9abf11827af3aff29
alex <HuiF8chi>
2022-03-25 10:21:13 +0200

    hook: useCallback

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {Component, useState, useEffect} from 'react';
+import {Component, useState, useEffect, useCallback} from 'react';
 import {Container} from 'react-bootstrap';
 import './App.css';
 // class Slider extends Component {
@@ -75,6 +75,15 @@ const Slider = (props) => {
 
     }, [slide]);
 
+    const getSomeImages = useCallback(() => {
+        console.log('fetching');
+    
+        return [
+            "https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg",
+            "https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg"
+        ]
+    }, []);
+
     function changeSlide(i) {
         setSlide(slide => slide + i);
     }
@@ -85,7 +94,7 @@ const Slider = (props) => {
     return (
         <Container>
             <div className="slider w-50 m-auto">
-                <img className="d-block w-100" src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg" alt="slide" />
+                <Slide getSomeImages={getSomeImages}/>
                 <div className="text-center mt-5">Active slide {slide} <br/>{autoplay ? 'auto' : null} </div>
                 <div className="buttons mt-3">
                     <button 
@@ -103,6 +112,19 @@ const Slider = (props) => {
     )
 }
 
+const Slide = ({getSomeImages}) => {
+    const [images, setImages] = useState([]);
+
+    useEffect(() => {
+        setImages(getSomeImages());
+    }, [getSomeImages]);
+
+    return (
+        <>
+            {images.map((url, i) => <img key={i} className="d-block w-100" src={url} alt="slide"/>)}
+        </>
+    );
+}
 
 function App() {
     const [slider, setSlider] = useState(true);
